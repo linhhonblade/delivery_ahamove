f038fede31986b749196bfe5675fc31e56317f3b
linhhonblade <meomaimaimeo@gmail.com>
2020-12-29 15:28:07 +0700

    create a config parameter to save ahamove token

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -6,6 +6,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     token_ahamove = fields.Char("Ahamove Token", config_parameter='ahamove_api_token',
-                         default='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhaGEiLCJ0eXAiOiJ1c2VyIiwiY2lkIjoiODQ4Mzg0Mzk0ODMiLCJzdGF0dXMiOiJPTkxJTkUiLCJlb2MiOm51bGwsIm5vYyI6IlwiUGhhbSBUaGkgTmdvYyBNYWlcIiIsImN0eSI6IkhBTiIsImFjY291bnRfc3RhdHVzIjoiQUNUSVZBVEVEIiwiZXhwIjoxNjA4NzE1NzUyLCJwYXJ0bmVyIjoiYXJyb3doaXRlY2gifQ.uTDVjsMfx6aqhh-k7nVo-hGyBzklGH0ZZ3L_QnnA2-M')
+                         default='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhaGEiLCJ0eXAiOiJ1c2VyIiwiY2lkIjoiODQ4Mzg0Mzk0ODMiLCJzdGF0dXMiOiJPTkxJTkUiLCJlb2MiOm51bGwsIm5vYyI6IlwiUGhhbSBUaGkgTmdvYyBNYWlcIiIsImN0eSI6IkhBTiIsImFjY291bnRfc3RhdHVzIjoiQUNUSVZBVEVEIiwiZXhwIjoxNjA5MjExNDUzLCJwYXJ0bmVyIjoiYXJyb3doaXRlY2gifQ.wDMPf78V7xH3x74T1nRmnWME5Cquf5KsMXJLbLIpWKo')
     refresh_token = fields.Char("Ahamove Refresh Token",
                                 config_parameter='ahamove_refresh_token', default='df27098effd1338af01581511f92be4b')
